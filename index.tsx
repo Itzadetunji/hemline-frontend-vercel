cbb884b06027d5b43a4f35ac5ec06c334fc5f808
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2025-10-13 22:17:14 +0100

    fix: Correct import path for Routes and update rendering syntax in Document component

--- index.tsx
+++ index.tsx
@@ -3,13 +3,11 @@ import { Router, Route, LocationProvider, ErrorBoundary } from "preact-iso";
 import { QueryClientProvider } from "@tanstack/react-query";
 
 import { queryClient } from "./src/lib/queryClient";
-import { Routes } from "@/layout/Routes";
+import { Routes } from "./src/layout/Routes";
 
 const Document = () => {
 	return (
-		<QueryClientProvider client={queryClient}>
-			{Routes() as any}
-		</QueryClientProvider>
+		<QueryClientProvider client={queryClient}>{<Routes />}</QueryClientProvider>
 	);
 };
 
