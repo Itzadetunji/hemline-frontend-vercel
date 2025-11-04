fe125e9339f1f38a35253083f312d87e9f579309
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2025-11-04 10:09:37 +0100

    feat: Refactor Routes component to improve structure and add Coming Soon page

--- src/layout/Routes.tsx
+++ src/layout/Routes.tsx
@@ -3,40 +3,64 @@ import { ErrorBoundary, LocationProvider, Route, Router } from "preact-iso";
 import { Onboarding } from "@/pages/Auth/Onboarding/page";
 import { SignIn } from "@/pages/Auth/SignIn/page";
 import { SignUp } from "@/pages/Auth/SignUp/page";
-import { Folders } from "@/pages/Folders/page";
-import { Gallery } from "@/pages/Gallery/page";
-import { ProtectedRoute } from "./ProtectedRoute";
-import { SingleFolderGallery } from "@/pages/Folders/single-folder";
-import { PublicFolderGallery } from "@/pages/Folders/public-single-folder";
-import { Profile } from "@/pages/Profile/page";
-import { Clients } from "@/pages/Clients/page";
-import { Orders } from "@/pages/Clients/orders/page";
 import { AddClients } from "@/pages/Clients/add-clients";
+import { Orders } from "@/pages/Clients/orders/page";
+import { Clients } from "@/pages/Clients/page";
+import { Folders } from "@/pages/Folders/page";
+import { PublicFolderGallery } from "@/pages/Folders/public-single-folder";
+import { SingleFolderGallery } from "@/pages/Folders/single-folder";
+import { Gallery } from "@/pages/Gallery/page";
+import { Profile } from "@/pages/Profile/page";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 export const Routes = () => {
   return (
     <LocationProvider>
       <ErrorBoundary>
-        <Router>
-          {/* Protected Routes - Require Authentication */}
+        {/* Protected Routes - Require Authentication */}
 
-          <ProtectedRoute path="/" component={Gallery} />
-          <ProtectedRoute path="/onboarding" component={Onboarding} />
+        {import.meta.env.MODE === "development" ? (
+          <Router>
+            <ProtectedRoute path="/" component={Gallery} />
+            <Route path="/sign-up" component={SignUp} />
+            <Route path="/sign-in" component={SignIn} />
+            <ProtectedRoute path="/onboarding" component={Onboarding} />
+            <ProtectedRoute path="/gallery" component={Gallery} />
+            <ProtectedRoute path="/gallery/folders" component={Folders} />
+            <ProtectedRoute path="/gallery/folders/:folder_id" component={SingleFolderGallery} />
+            <Route path="/folders/:public_id" component={PublicFolderGallery} />
+            <ProtectedRoute path="/clients" component={Clients} />
+            <ProtectedRoute path="/clients/add" component={AddClients} />
+            <ProtectedRoute path="/clients/orders" component={Orders} />
+            <ProtectedRoute path="/profile" component={Profile} />
+            <ProtectedRoute path="/onboarding" component={Onboarding} />
+          </Router>
+        ) : (
+          <Router>
+            <Route path="/" component={ComingSoon} />
+            <Route path="/" component={ComingSoon} />
+          </Router>
+        )}
 
-          {/* Public Routes - Redirect if already authenticated */}
-          <Route path="/sign-up" component={SignUp} />
-          <Route path="/sign-in" component={SignIn} />
-          <ProtectedRoute path="/onboarding" component={Onboarding} />
-          <ProtectedRoute path="/gallery" component={Gallery} />
-          <ProtectedRoute path="/gallery/folders" component={Folders} />
-          <ProtectedRoute path="/gallery/folders/:folder_id" component={SingleFolderGallery} />
-          <Route path="/folders/:public_id" component={PublicFolderGallery} />
-          <ProtectedRoute path="/clients" component={Clients} />
-          <ProtectedRoute path="/clients/add" component={AddClients} />
-          <ProtectedRoute path="/clients/orders" component={Orders} />
-          <ProtectedRoute path="/profile" component={Profile} />
-        </Router>
+        {/* Public Routes - Redirect if already authenticated */}
       </ErrorBoundary>
     </LocationProvider>
+  );
+};
+
+const ComingSoon = () => {
+  return (
+    <div class="flex h-screen flex-1 flex-col p-4">
+      <ul class="flex items-center gap-4">
+        <img src="/assets/brand/logo.svg" class="size-8" alt="Brand Logo" />
+        <h1 class="max-w-[10ch] truncate pr-1 text-3xl text-black">Hemline</h1>
+      </ul>
+      <div class="flex flex-1 flex-col items-center justify-center gap-2">
+        <h2 class="text-4xl">Coming Soon</h2>
+        <a href="https://x.com/hemlinestudio">
+          Check us out <span class="underline">here</span>
+        </a>
+      </div>
+    </div>
   );
 };
